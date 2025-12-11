19e4a4c62cae9678ab928a6a7beda122fe92d439
c11gunworks-lab <c11gunworks@gmail.com>
2025-12-11 10:35:01 -0700

    Startup

---
+++ unpractical_tactical/Program.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Extensions.Logging;
+using SPTarkov.DI.Annotations;
+using SPTarkov.Server.Core.DI;
+using SPTarkov.Server.Core.Models.Spt.Mod;
+using System.Reflection;
+using Range = SemanticVersioning.Range;
+
+namespace c11_tn_4;
+
+public record ModMetadata : AbstractModMetadata
+{
+    public override string ModGuid { get; init; } = "com.c11.truenorth4";
+    public override string Name { get; init; } = "True North";
+    public override string Author { get; init; } = "C11";
+    public override SemanticVersioning.Version Version { get; init; } = new("1.0.0");
+
+    // Allow any 4.0.x 
+    public override Range SptVersion { get; init; } = new("^4.0.0");
+
+    public override string License { get; init; } = "MIT";
+    public override bool? IsBundleMod { get; init; } = true;
+
+    public override Dictionary<string, Range>? ModDependencies { get; init; } = new()
+    {
+        { "com.wtt.commonlib", new Range("~2.0.0") }
+    };
+
+    public override string? Url { get; init; }
+    public override List<string>? Contributors { get; init; }
+    public override List<string>? Incompatibilities { get; init; }
+}
+
+[Injectable(TypePriority = OnLoadOrder.PostDBModLoader + 2)]
+public class TrueNorth(
+    WTTServerCommonLib.WTTServerCommonLib wttCommon,
+    ILogger<TrueNorth> log
+) : IOnLoad
+{
+    public async Task OnLoad()
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+
+        // Log resource names once while wiring things up
+        foreach (var name in assembly.GetManifestResourceNames())
+            log.LogDebug("[TrueNorth] Embedded resource: {Res}", name);
+
+        // WTT ingestion
+        await wttCommon.CustomItemServiceExtended.CreateCustomItems(assembly);
+        await wttCommon.CustomLocaleService.CreateCustomLocales(assembly);
+        await wttCommon.CustomAssortSchemeService.CreateCustomAssortSchemes(assembly);
+
+
+        log.LogInformation("Welcome to the True North");
+        await Task.CompletedTask;
+    }
+}
